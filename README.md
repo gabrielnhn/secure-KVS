6cad83bb4396c7982142938f32cbaf67d4986754
RichardHeise <richardfhf5@gmail.com>
2023-05-16 18:45:03 -0300

    Server inicio

---
+++ README.md
@@ -0,0 +1,7 @@
+# What to do?
+ 
+ - Python client 
+ - Python server with redis (redis-py)
+ - CRUD (CREATE, READ, UPDATE, DELETE)
+ - Use TLS to make communication secure
+ - YouTube video
---
+++ server.py
@@ -0,0 +1,29 @@
+import redis
+import socket
+
+HOST = '127.0.0.1'
+PORT = 5050
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    print(f"Listening on port {PORT}...")
+    s.listen()
+
+    conn, addr = s.accept()
+
+    with conn:
+            print(f">>> Server {PORT} connected to client on socket {addr}")
+            while True:
+                data = conn.recv(1024)
+                data = data.decode("utf-8")
+                if not data:
+                    s.close()
+                    print("NO connection. Finishing...")
+                    break
+
+            print(f"Received request '{data}'")
+
+if __name__ == "__main__":
+    main()
