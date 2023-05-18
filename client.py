544926a76381707b07766343ff63d349441b411a
RichardHeise <richardfhf5@gmail.com>
2023-05-18 11:41:42 -0300

    Redis feito, talvez alterar update

--- client.py
+++ client.py
@@ -1,7 +1,42 @@
 import socket
+import json
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 5050  # The port used by the server cache
+PORT = 5050  # The port used by the server
+
+def insert():
+    key = input("Please select the key to insert: ")
+    value = input("Please select the value to insert: ")
+    return {"opcode": 1, "key": key, "value": value}
+
+def read():
+    key = input("Please select the key (key) to read: ")
+    return {"opcode": 2, "key": key}
+
+def update():
+    key = input("Please select the key to update:")
+    value = input("Please select the value to update: ")
+    return {"opcode": 3, "key": key, "value": value}
+
+def delete():
+    key = input("Please select the key (key) to delete: ")
+    return {"opcode": 4, "key": key}
+
+
+def operation():
+    while True:
+        opcode = input("What operation would you like to do?\n1.Insert.\n2.Read\n3.Update\n4.Delete\nPlease select a number: ")
+        match int(opcode):
+            case 1:
+                return insert()
+            case 2:
+                return read()
+            case 3:
+                return update()
+            case 4:
+                return delete()
+            case _:
+                print("Operation not permited, plase try again.")
 
 def main():
 
@@ -9,12 +44,12 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        # Try to connect to Cache Server
+        # Try to connect to server Server
         try:
             s.connect((HOST, PORT))
         except ConnectionRefusedError:
             s.close()
-            print("Error: Could not connect to CACHE server")
+            print("Error: Could not connect to server server")
             exit()
         
         print(f"Connected to server on port {PORT}...")
@@ -22,7 +57,7 @@ def main():
 
         # Client input
         while True:
-            req = input(">>> ")
+            req = json.dumps(operation())
 
             data = req.encode('UTF-8') # Encode data
 
@@ -30,13 +65,13 @@ def main():
 
             s.settimeout(1.0) # Timeout (1 second)
 
-            # Try to wait for an answer from Cache Server
+            # Try to wait for an answer from Server
             try:
-                data = s.recv(1024) # Received data from Cache Server (buffer 1024 bytes)
-                data = data.decode("utf-8") # Decode data
-                print(data)
+                data = s.recv(1024) # Received data from server (buffer 1024 bytes)
+                data = json.loads(data.decode("utf-8")) # Decode data
+                print(data['res'])
             except socket.timeout:
-                print("No data was received from cache.")
+                print("No data was received from server.")
             except:
                 s.close()
 
--- server.py
+++ server.py
@@ -1,32 +1,54 @@
 import redis
+import json
 import socket
 
-HOST = '127.0.0.1'
-PORT = 5050
+HOST = '127.0.0.1'  # IP address to bind the socket to
+PORT = 5050  # Port number to listen on
+bd = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+def process_recv(data):
+    match int(data['opcode']):
+        case 1:
+            bd.set(data['key'], data['value'])
+            return {"res": "Inserted"}
+        case 2:
+            value = bd.get(data['key'])
+            return {"res": value}
+        case 3:
+            # oldValue = bd.get(data['key'])
+            bd.delete(data['key'])
+            bd.set(data['key'], data['value'])
+            return {"res": f"Value update to {data['value']}"}
+        case 4:
+            value = bd.delete(data['key'])
+            return {"res": f"Deleted at {data['key']}"}
+        case _:
+            print("Invalid opcode")
+            return {"res": "Invalid opcode"}
+
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP socket
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Set socket options
+    s.bind((HOST, PORT))  # Bind the socket to the specified IP address and port
     print(f"Listening on port {PORT}...")
-    s.listen()
+    s.listen()  # Listen for incoming connections
 
-    conn, addr = s.accept()
+    conn, addr = s.accept()  # Accept a connection from a client
 
     with conn:
-            print(f">>> Server {PORT} connected to client on socket {addr}")
-            while True:
-                data = conn.recv(1024)
-                data = data.decode("utf-8")
-                if not data:
-                    s.close()
-                    print("NO connection. Finishing...")
-                    break
+        print(f">>> Server {PORT} connected to client on socket {addr}")
+        while True:
+            data = conn.recv(1024)  # Receive data from the client (maximum of 1024 bytes)
+            data = json.loads(data.decode("utf-8"))  # Decode the received data from bytes to string
+            if not data:
+                s.close()  # Close the socket
+                print("NO connection. Finishing...")
+                break
 
-                print(f"Received request '{data}'")
-                response = "Operation received"
-                # do stuff
-                conn.sendall(response.encode("utf-8"))
+            print(f"Received request '{data}'")
+            response = process_recv(data)
+            conn.sendall(json.dumps(response).encode("utf-8"))  # Send the response back to the client
 
 if __name__ == "__main__":
     main()
