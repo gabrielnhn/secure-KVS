ef87b4f218fd4f3c8e16a15c18759b7e74eb2c46
RichardHeise <richardfhf5@gmail.com>
2023-05-30 18:59:48 -0300

    Tentando modificar

--- man.py
+++ man.py
@@ -7,29 +7,31 @@ cliente.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 cliente.bind(('localhost', 5051))
 cliente.listen(1)
 cliente_conn, addr = cliente.accept()  # Aceita uma conexão de um cliente
-cliente_conn.settimeout(0.5)
-cliente.settimeout(0.5)
+cliente_conn.settimeout(0.1)
+cliente.settimeout(0.1)
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.connect(('localhost', 5050))
-servidor.settimeout(0.5)
+servidor.settimeout(0.1)
 
 def modify_bit(data, bit_index):
-    # byte_index = bit_index // 8
-    # bit_offset = bit_index % 8
+    byte_index = bit_index // 8
+    bit_offset = bit_index % 8
 
-    # byte_data = bytearray(data)
-    # byte_data[byte_index] ^= (1 << bit_offset)
+    byte_data = bytearray(data)
+    byte_data[byte_index] ^= (1 << bit_offset)
 
-    # return bytes(byte_data)
-    return data
+    return bytes(byte_data)
 
 def main():
-
+    count = 3
     while True:
 
         try:
             pacote = cliente_conn.recv(1024)
+
+            if count % 9 == 0:
+                pacote = modify_bit(pacote, 5)
         except: 
             pacote = None
         print("Recebeu do cliente: ")
@@ -45,5 +47,7 @@ def main():
 
         if pacote:
             cliente_conn.sendall(pacote)
+
+        count += 1
 
 main()
